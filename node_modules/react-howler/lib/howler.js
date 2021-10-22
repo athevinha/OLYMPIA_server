4af997f69e9d30f17efc58a07b9faadcd34be91e
athevinha <nguyenthevinh1002@gmail.com>
2021-10-22 16:49:07 +0700

    ok

--- node_modules/react-howler/lib/howler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Howler = void 0;
-
-if (typeof window !== 'undefined') {
-  Howler = require('howler');
-}
-
-module.exports = Howler;
--- node_modules/react-howler/lib/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-/**
- * Noop function (do nothing)
- */
-var noop = exports.noop = function noop() {};
